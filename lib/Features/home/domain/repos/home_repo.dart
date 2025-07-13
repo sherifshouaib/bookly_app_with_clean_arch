4fe111f6f022fe8c9248dc549ca97fc3b2111998
sherifshouaib <sherifshouaib22@gmail.com>
2025-07-13 18:12:19 +0300

    home repo

--- lib/Features/home/domain/repos/home_repo.dart
+++
@@ -1,6 +0,0 @@
-import 'package:bookly/Features/home/domain/entities/book_entity.dart';
-
-abstract class HomeRepo {
-  Future<List<BookEntity>> fetchFeaturedBooks();
-  Future<List<BookEntity>> fetchNewestBooks();
-}
